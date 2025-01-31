1bbe63a1daf9dd00ccecaa99b5d7e3e8a00c7599
Nathan Angelo Stenlie <stenlieangelonathan@gmail.com>
2025-01-31 17:03:32 +0700

    Setup wallet service and controller
    making create method

--- service-wallet/src/main/java/com/example/service_wallet/controller/WalletController.java
+++ service-wallet/src/main/java/com/example/service_wallet/controller/WalletController.java
@@ -1,5 +1,24 @@
 package com.example.service_wallet.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.base_domain.dtos.WalletDTO;
+import com.example.service_wallet.service.WalletService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/wallet")
+@RequiredArgsConstructor
 public class WalletController {
-    
+
+    private final WalletService walletService;
+
+    @PostMapping
+    public ResponseEntity<Object> createWallet(WalletDTO walletDTO) throws Exception {
+        return walletService.createWallet(walletDTO);
+    }
 }
--- service-wallet/src/main/java/com/example/service_wallet/service/WalletService.java
+++ service-wallet/src/main/java/com/example/service_wallet/service/WalletService.java
@@ -1,15 +1,12 @@
 package com.example.service_wallet.service;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import com.example.base_domain.dtos.WalletDTO;
 import com.example.base_domain.entities.Wallet;
 import com.example.base_domain.repositories.WalletRepository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,15 +19,14 @@ public class WalletService {
         return ResponseEntity.ok(walletRepository.findAll());
     }
 
-    @JmsListener(destination = "createWallet")
-    @Transactional
-    public void createWallet(@Payload WalletDTO walletDTO) throws Exception {
+    public ResponseEntity<Object> createWallet(WalletDTO walletDTO) throws Exception {
         try {
             Wallet wallet = Wallet.builder()
                     .id(walletDTO.getId())
                     .balance(walletDTO.getBalance())
                     .build();
             walletRepository.save(wallet);
+            return ResponseEntity.ok(wallet);
         } catch (Exception e) {
             throw new Exception("4000");
         }
